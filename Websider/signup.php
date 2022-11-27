2c80deba740c4c5be34588be8fdb0de9d67c4f65
Bman231 <112167920+Bman231@users.noreply.github.com>
2022-11-27 21:08:43 +0100

    Add files via upload

---
+++ Websider/signup.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'brukere';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	// If there is an error with the connection, stop the script and display the error.
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+// Now we check if the data from the login form was submitted, isset() will check if the data exists.
+if ( !isset($_POST['username'], $_POST['password']) ) {
+	// Could not get the data that should have been sent.
+	exit('Please fill both the username and password fields!');
+}
+
+// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+if ($stmt = $con->prepare('INSERT INTO brukere (username, password) VALUES ($_POST("username"), $_POST("password")')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+	// Store the result so we can check if the account exists in the database.
+	$stmt->store_result();
+
+	/*$stmt->close();*/
+
+    // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+        session_regenerate_id();
+        $_SESSION['loggedin'] = TRUE;
+        $_SESSION['name'] = $_POST['username'];
+        $_SESSION['id'] = $id;
+        header('Location: home.php');
+}
+?>
