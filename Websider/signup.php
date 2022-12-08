b1f478e2f47a8850a4c1a9e2677512f06507086a
Bman231 <112167920+Bman231@users.noreply.github.com>
2022-12-08 19:17:22 +0100

    Update signup.php

--- Websider/signup.php
+++ Websider/signup.php
@@ -1,34 +1,33 @@
 <?php
 session_start();
-// Change this to your connection info.
+// Connection detaljer til databasen
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'brukere';
-// Try and connect using the info above.
+// Koden kobler php-filen til datbasen
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
+	// Klarte ikke koble til databasen. Viser feilmelding
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-// Now we check if the data from the login form was submitted, isset() will check if the data exists.
+// Sjekker om data fra formen ble submitted, isset() sjekker om dataen finnes i feltene.
 if ( !isset($_POST['username'], $_POST['password']) ) {
-	// Could not get the data that should have been sent.
-	exit('Please fill both the username and password fields!');
+	// Feltene var ikke fylt ut.
+	exit('Fyll ut begge feltene i registreringsskjemaet!');
 }
 
-// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('INSERT INTO brukere (username, password) VALUES ($_POST("username"), $_POST("password")')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+$username = $_POST['username'];
+$pass = $_POST['password'];
+
+// Forbereder SQL-injection. Prepare forhinder at dataene blir sendt til databasen hvis det er noe feil
+if  ($stmt = $con->prepare('INSERT INTO accounts (username, password) VALUES ('$username', '$pass')')) {
+	// Setter "s" i brukernavnet slik at vi vet at den er en string
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
-	// Store the result so we can check if the account exists in the database.
-	$stmt->store_result();
 
-	/*$stmt->close();*/
-
-    // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+    // Oppretter session. Virker som cookies, men husker dataene på serveren frem til brukeren logger ut
         session_regenerate_id();
         $_SESSION['loggedin'] = TRUE;
         $_SESSION['name'] = $_POST['username'];
